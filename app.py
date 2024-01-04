9dda76b0797659e6c0bdea65fb4dda8c0ca3d064
Connie <c.amer@icloud.com>
2024-01-04 16:20:08 +0000

    clean up + improve frontend

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 import tensorflow_hub as hub 
@@ -18,13 +18,12 @@ model = tf.keras.models.load_model(
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
-# Bootstrap-Flask requires this line
 bootstrap = Bootstrap5(app)
-# Flask-WTF requires this line
+
 csrf = CSRFProtect(app)
 
 class NameForm(FlaskForm):
-    text = StringField("Text", validators=[DataRequired(), Length(5, 40)])
+    text = TextAreaField("", validators=[DataRequired(), Length(5)])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -40,8 +39,6 @@ def index():
             return render_template("results.html", result="Spam", prediction=prediction)
         else:
             return render_template("results.html", result="Not Spam", prediction=prediction)
-    else:
-        message = "Please enter a valid text."
 
     return render_template("index.html", form=form, message=message)
 
